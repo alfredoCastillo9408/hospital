eae70f6672ae12db52bdb5e9da4dfd7100c26b8e
alfredoCastillo9408 <castillojesush@outlook.com>
2024-01-29 13:10:06 -0600

    Create DTOs to manage responses to send to controller

--- src/main/java/com/personal/castillo/hospital/dto/AppointmentDTO.java
+++ src/main/java/com/personal/castillo/hospital/dto/AppointmentDTO.java
@@ -1,5 +1,68 @@
 package com.personal.castillo.hospital.dto;
 
 public class AppointmentDTO {
-    
+    String pacientName;
+    String dateSchedule;
+    String doctorName;
+    String officeNumber;
+    String floorOffice;
+
+    public AppointmentDTO() {
+    }
+
+    public AppointmentDTO(String pacientName, String dateSchedule, String doctorName, String officeNumber,
+            String floorOffice) {
+        this.pacientName = pacientName;
+        this.dateSchedule = dateSchedule;
+        this.doctorName = doctorName;
+        this.officeNumber = officeNumber;
+        this.floorOffice = floorOffice;
+    }
+
+    public String getPacientName() {
+        return pacientName;
+    }
+
+    public void setPacientName(String pacientName) {
+        this.pacientName = pacientName;
+    }
+
+    public String getDateSchedule() {
+        return dateSchedule;
+    }
+
+    public void setDateSchedule(String dateSchedule) {
+        this.dateSchedule = dateSchedule;
+    }
+
+    public String getDoctorName() {
+        return doctorName;
+    }
+
+    public void setDoctorName(String doctorName) {
+        this.doctorName = doctorName;
+    }
+
+    public String getOfficeNumber() {
+        return officeNumber;
+    }
+
+    public void setOfficeNumber(String officeNumber) {
+        this.officeNumber = officeNumber;
+    }
+
+    public String getFloorOffice() {
+        return floorOffice;
+    }
+
+    public void setFloorOffice(String floorOffice) {
+        this.floorOffice = floorOffice;
+    }
+
+    @Override
+    public String toString() {
+        return "AppointmentDTO [pacientName=" + pacientName + ", dateSchedule=" + dateSchedule + ", doctorName="
+                + doctorName + ", officeNumber=" + officeNumber + ", floorOffice=" + floorOffice + "]";
+    }
+
 }
--- src/main/java/com/personal/castillo/hospital/service/AppointmentService.java
+++ src/main/java/com/personal/castillo/hospital/service/AppointmentService.java
@@ -1,8 +1,51 @@
 package com.personal.castillo.hospital.service;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.personal.castillo.hospital.dto.AppointmentDTO;
+import com.personal.castillo.hospital.iservice.IAppointmentService;
+import com.personal.castillo.hospital.model.Appointment;
+import com.personal.castillo.hospital.repository.AppointmentRepository;
+
 @Service
-public class AppointmentService {
+public class AppointmentService implements IAppointmentService{
+
+    @Autowired
+    AppointmentRepository _repository;
+
+    @Override
+    public List<AppointmentDTO> getAllAppointments() {
+        Iterable<Appointment> app = _repository.findAll();
+
+        return new ArrayList<>();
+    }
+
+    @Override
+    public List<AppointmentDTO> getAppointmentsBySchedule(LocalDateTime dateToSearch) {
+        throw new UnsupportedOperationException("Unimplemented method 'getAppointmentsBySchedule'");
+    }
+
+    @Override
+    public List<AppointmentDTO> getAppointmentsByDoctoroOffice(int officeNumber) {
+        
+        throw new UnsupportedOperationException("Unimplemented method 'getAppointmentsByDoctoroOffice'");
+    }
+
+    @Override
+    public List<AppointmentDTO> getAppointmentsByDoctorsName(String doctorsName) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAppointmentsByDoctorsName'");
+    }
+
+    @Override
+    public boolean deleteAppointment(long idAppointment) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteAppointment'");
+    }
     
 }
